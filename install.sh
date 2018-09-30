41ff03d143eb3fb56d8b32191cb07b56da4011d1
Kevin Hui <>
2018-09-30 15:57:01 +0800

    add tmux

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ makepkg -si
 cd ..
 rm -rf yay/
 echo -e "${GREEN}Installing zsh and oh-my-zsh...${NC}"
-sudo pacman -S zsh
+sudo pacman -S --noconfirm zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sed 's:env zsh -l::g')"
 echo -e "${GREEN}Installing powerline patched fonts...${NC}"
 git clone https://github.com/powerline/fonts.git --depth=1
@@ -21,5 +21,10 @@ cd ..
 rm -rf fonts
 echo -e "${GREEN_L}Powerline patched fonts installed. You can now change your font in Preference."
 echo -e "${GREEN}Installing zsh-theme-powerlevel9k...${NC}"
-sudo pacman -S zsh-theme-powerlevel9k
+sudo pacman -S --noconfirm zsh-theme-powerlevel9k
 echo 'source /usr/share/zsh-theme-powerlevel9k/powerlevel9k.zsh-theme' >> ~/.zshrc
+echo -e "${GREEN}Installing tmux...${NC}"
+sudo pacman -S --noconfirm tmux
+echo 'export TERM="xterm-256color"' >> ~/.zshrc
+echo 'set -g default-terminal "screen-256color"' >> ~/.tmux.conf
+echo 'set-window-option -g mode-keys vi' >> ~/.tmux.conf
