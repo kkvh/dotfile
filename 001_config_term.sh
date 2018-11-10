4e896a3548e23d1fa812028bab9c1e56b2e479b9
Kevin Hui <8171598+kevinhui@users.noreply.github.com>
2018-11-10 23:44:39 +0800

    Add source sans pro fonts

--- 001_config_term.sh
+++ 001_config_term.sh
@@ -24,6 +24,8 @@ run "Installing Meslo Nerd Font..."
 sudo mkdir /usr/share/fonts/NerdFonts
 sudo cp "./Meslo LG M Regular Nerd Font Complete.otf" /usr/share/fonts/NerdFonts/
 fc-cache -vf
+run "Installing Source Sans Pro Fonts..."
+yay_nc adobe-source-sans-pro-fonts
 
 # powerlevel9k
 run "Installing zsh-theme-powerlevel9k..."
